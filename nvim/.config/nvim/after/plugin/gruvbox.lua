4bc6b39d2d43b2bf30a10831be61179d83b7fcca
r3mcos3 <r3mcos3@gmail.com>
2022-12-22 17:47:57 +0100

    add transparency & description

--- nvim/.config/nvim/after/plugin/gruvbox.lua
+++ nvim/.config/nvim/after/plugin/gruvbox.lua
@@ -1,2 +1,8 @@
+
+-- Colorscheme
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+-- Transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
